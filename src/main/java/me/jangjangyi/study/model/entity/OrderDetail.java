7430769c48390df7959fe101852cfaf270cf1672
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-01-11 20:19:28 +0900

    partner item order detail테이블 테스트

--- src/main/java/me/jangjangyi/study/model/entity/OrderDetail.java
+++ src/main/java/me/jangjangyi/study/model/entity/OrderDetail.java
@@ -20,8 +20,6 @@ public class OrderDetail {
 
     private String status;
 
-    private LocalDateTime orderAt;
-
     private LocalDateTime arrivalDate;
 
     private Integer quantity;
@@ -35,4 +33,8 @@ public class OrderDetail {
     private LocalDateTime updatedAt;
 
     private String updatedBy;
+
+    private Long itemId;
+
+    private Long orderGroupId;
 }
--- src/test/java/me/jangjangyi/study/repository/OrderDetailRepositoryTest.java
+++ src/test/java/me/jangjangyi/study/repository/OrderDetailRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -24,9 +25,21 @@ class OrderDetailRepositoryTest {
 
     @Test
     public void create() {
+        OrderDetail orderDetail = new OrderDetail();
 
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(1000000));
 
+        orderDetail.setOrderGroupId(1L);
+        orderDetail.setItemId(1L);
 
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+        Assertions.assertThat(newOrderDetail).isNotNull();
 
     }
     @Test
