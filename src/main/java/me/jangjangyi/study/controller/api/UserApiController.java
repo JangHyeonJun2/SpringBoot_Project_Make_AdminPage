f783eb9246729fba803dc183f9988bcf7a0e1e4f
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-01-14 22:11:08 +0900

    사용자 API Create

--- src/main/java/me/jangjangyi/study/controller/api/UserApiController.java
+++ src/main/java/me/jangjangyi/study/controller/api/UserApiController.java
@@ -1,28 +1,38 @@
 package me.jangjangyi.study.controller.api;
 
+import lombok.extern.slf4j.Slf4j;
 import me.jangjangyi.study.ifs.CrudInterface;
 import me.jangjangyi.study.model.network.Header;
+import me.jangjangyi.study.model.network.request.UserApiRequest;
+import me.jangjangyi.study.model.network.response.UserApiResponse;
+import me.jangjangyi.study.service.UserApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest,UserApiResponse>  {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}", request);
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}") // /api/user/{id}
-    public Header read(@PathVariable(name = "id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("") // /api/user
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         return null;
     }
 
--- src/main/java/me/jangjangyi/study/ifs/CrudInterface.java
+++ src/main/java/me/jangjangyi/study/ifs/CrudInterface.java
@@ -2,12 +2,12 @@ package me.jangjangyi.study.ifs;
 
 import me.jangjangyi.study.model.network.Header;
 
-public interface CrudInterface {
-    Header create(); //todo request Object 추가
+public interface CrudInterface<Req,Res> {
+    Header<Res> create(Header<Req> request); //todo request Object 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 }
